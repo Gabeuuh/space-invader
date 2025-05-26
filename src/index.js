3bc3dbafa057d335d1bf004e5ed685c466f9790e
Gabin Clavier <gabin.clavier22@gmail.com>
2025-05-26 13:51:49 +0200

    3.3.h.affichage du player

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import '../css/styles.scss';
 import Space from '../assets/images/space.png';
-
+import Player from './player';
 import InvaderController from './invaderController';
 
 
 let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d');
+const player = new Player(canvas, 3);
 canvas.width = 600;
 canvas.height = 600;
 const invaderController = new InvaderController(canvas);
@@ -13,6 +14,7 @@ const invaderController = new InvaderController(canvas);
 function game(){
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     invaderController.draw(ctx);
+    player.draw(ctx);
 }
 setInterval(game, 1000/60);
 
---
+++ src/player.js
@@ -0,0 +1,22 @@
+import PlayerSprite from '../assets/images/player.png';
+
+export default class Player {
+  constructor(canvas, velocity) {
+    console.log(canvas.width, "CANVAS WIDTH");
+    console.log(canvas.height, "CANVAS HEIGHT");
+    this.canvas = canvas;
+    this.velocity = velocity;
+
+    this.x = canvas.width/2; 
+    this.y = canvas.height - 25;
+    this.width = 40;
+    this.height = 40;
+    this.image = new Image();
+    this.image.src = PlayerSprite;
+
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
